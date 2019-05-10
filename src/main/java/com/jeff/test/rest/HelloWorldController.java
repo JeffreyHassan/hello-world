5eb00d269f0c4233704b05d9c6b1526a94748cf5
Jeff <jeffrey.hassan@stern.nyu.edu>
2019-05-10 10:08:08 -0400

    Adding small change to test pipeline

--- src/main/java/com/jeff/test/rest/HelloWorldController.java
+++ src/main/java/com/jeff/test/rest/HelloWorldController.java
@@ -7,16 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-/**
- * Created by Jeff on 5/10/2019.
- */
+
 @RestController
 public class HelloWorldController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @RequestMapping("/")
+    public ResponseEntity<String> helloWorld(@RequestParam(value="name", defaultValue="World") String name) {
+        return ResponseEntity.ok("Hello World!");
+    }
+
     @RequestMapping("/hello")
     public ResponseEntity<String> hello(@RequestParam(value="name", defaultValue="World") String name) {
-        return ResponseEntity.ok(name);
+        return ResponseEntity.ok(String.format(template, name));
     }
 }
