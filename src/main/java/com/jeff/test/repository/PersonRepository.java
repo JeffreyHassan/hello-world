b4af934c2aefd80ae3172a895eb92e9f22418c76
Jeff <jeffrey.hassan@stern.nyu.edu>
2019-05-15 10:03:34 -0400

    Add another dumb query

--- src/main/java/com/jeff/test/repository/PersonRepository.java
+++ src/main/java/com/jeff/test/repository/PersonRepository.java
@@ -3,6 +3,8 @@ package com.jeff.test.repository;
 import com.jeff.test.model.Person;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PersonRepository extends CrudRepository<Person, Integer> {
-    Person findByFullName(String name);
+    List<Person> findByFullName(String name);
 }
--- src/main/java/com/jeff/test/rest/PersonController.java
+++ src/main/java/com/jeff/test/rest/PersonController.java
@@ -4,10 +4,7 @@ import com.jeff.test.model.Person;
 import com.jeff.test.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +18,12 @@ public class PersonController {
     @Autowired
     public PersonController(PersonRepository personRepository) {
         this.personRepository = personRepository;
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<List<Person>> getPersonsByFullName(@PathVariable("name") String name) {
+        List<Person> personList = personRepository.findByFullName(name);
+        return ResponseEntity.ok(personList);
     }
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
